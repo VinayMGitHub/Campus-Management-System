a946278598edb869f91e4c6a65120ada697aa6e9
VinayMGitHub <114752040+VinayMGitHub@users.noreply.github.com>
2024-07-03 21:58:24 +0530

    student_router.py code refactored

--- backend/routers/student_router.py
+++ backend/routers/student_router.py
@@ -1,29 +1,33 @@
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
+from models import student_model
 
-student_router = APIRouter(
+router = APIRouter(
     prefix="/students",
     tags = ["student"],
 )
 
-class Student(BaseModel):
-    name: str
-    age: int
+# class Student(BaseModel):
+#     name: str
+#     age: int
 
-students_list = [{"name" : "Vinay", "age" : 21}, {"name" : "Manu", "age" : 22}, {"name" : "Sreevardhan", "age" : 23}, {"name" : "Ajith", "age" : 24}]
+# students_list = [{"name" : "Vinay", "age" : 21}, {"name" : "Manu", "age" : 22}, {"name" : "Sreevardhan", "age" : 23}, {"name" : "Ajith", "age" : 24}]
 
-@student_router.get("/")
+@router.get("/")
 def read_students():
-    return students_list
+    pass
+    # return students_list
 
-@student_router.get("/{name}")
+@router.get("/{name}")
 def read_students(name :str):
-    students_with_requested_age = [student for student in students_list if student["name"] == name]
-    return students_with_requested_age
+    # students_with_requested_age = [student for student in students_list if student["name"] == name]
+    # return students_with_requested_age
+    pass
 
-@student_router.post("/")
-def add_student(student : Student):
-    print(type(student))
-    new_student = student.dict()
-    students_list.append(new_student)
-    return new_student
+@router.post("/")
+def add_student(student : student_model.student):
+    pass
+    # print(type(student))
+    # new_student = student.dict()
+    # students_list.append(new_student)
+    # return new_student
