beeee0c8cd0ff9b7207259eccd0340c83ab6c6cb
VinayMGitHub <114752040+VinayMGitHub@users.noreply.github.com>
2024-07-17 16:13:10 +0530

    Table component created

---
+++ frontend/src/components/TableComponent.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { DataTable } from "carbon-components-svelte";
+  export let headers;
+  export let rows;
+</script>
+
+<DataTable
+  headers={headers}
+  rows={rows}
+/>
+
+
+<style></style>
--- frontend/src/routes/Teacher.svelte
+++ frontend/src/routes/Teacher.svelte
@@ -1,10 +1,45 @@
 <script>
+    import TableComponent from "../components/TableComponent.svelte"; 
+    let headers = [
+    { key: "id", value: "ID" },
+    { key: "name", value: "Name" },
+    { key: "gender", value: "Gender" },
+    { key: "position", value: "Position" },
+    ];
+
+    let rows = [
+    {
+      id: "1",
+      name: "Load Balancer 3",
+      gender: "HTTP",
+      position: 3000,
+    },
+    {
+      id: "2",
+      name: "Load Balancer 3",
+      gender: "HTTP",
+      position: 3000,
+    },
+    {
+      id: "3",
+      name: "Load Balancer 3",
+      gender: "HTTP",
+      position: 3000,
+    },
+    {
+      id: "4",
+      name: "Load Balancer 3",
+      gender: "HTTP",
+      position: 3000,
+    }
+    ];
 
 </script>
 
 <main>
     <h1>
-        Page for Teacher
+        <h1>Page for Teacher</h1>  
+        <TableComponent headers = {headers} rows = {rows}/>
     </h1>
 </main>
 
@@ -12,3 +47,5 @@
 <style>
 
 </style>
+
+
